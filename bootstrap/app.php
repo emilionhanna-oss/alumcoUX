071194604dffb83c967761e029ae2b09bc9e8ec7
emilionhanna-oss <emilionhanna@gmail.com>
2026-08-01 05:00:34 -0400

    Fix: agregar use Illuminate\Http\Request faltante

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,13 +21,13 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->trustProxies(   // ← NUEVO
-            at: '*',                  // ← NUEVO
-            headers: Request::HEADER_X_FORWARDED_FOR |   // ← NUEVO
-                Request::HEADER_X_FORWARDED_HOST |         // ← NUEVO
-                Request::HEADER_X_FORWARDED_PORT |         // ← NUEVO
-                Request::HEADER_X_FORWARDED_PROTO          // ← NUEVO
-        );                             // ← NUEVO
+        $middleware->trustProxies(   
+            at: '*',                
+            headers: Request::HEADER_X_FORWARDED_FOR |   
+                Request::HEADER_X_FORWARDED_HOST |         
+                Request::HEADER_X_FORWARDED_PORT |        
+                Request::HEADER_X_FORWARDED_PROTO         
+        );                          
 
         //
     })
